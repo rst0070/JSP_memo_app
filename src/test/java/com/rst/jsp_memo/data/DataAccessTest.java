2327ca94edfcb7ff0a6f11e6b189646cea8da043
rst0070 <kwb0711@gmail.com>
2021-10-18 11:18:42 +0000

    testing jdbc with sqlite

--- src/test/java/com/rst/jsp_memo/data/DataAccessTest.java
+++ src/test/java/com/rst/jsp_memo/data/DataAccessTest.java
@@ -10,16 +10,19 @@ public class DataAccessTest {
     Connection connection;
     Statement statement;
 	ResultSet resultSet;
-
+    private static final String DB_FILE = "jsp_memo.db";
     @BeforeAll
     public void init(){
         try{
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:");
+            connection = DriverManager.getConnection("jdbc:sqlite:"+DB_FILE);
+            System.out.println(connection.getSchema());
+            statement = connection.createStatement();
+            
         }catch(ClassNotFoundException ce){
             ce.printStackTrace();
-        }catch(SqlException se){
-            
+        }catch(SQLException se){
+            se.printStackTrace();
         }
         
 
@@ -27,6 +30,15 @@ public class DataAccessTest {
 
     @Test
     public void selectTest(){
+        try{
+            resultSet = statement.executeQuery("select * from `MEMO`");
+            String result = resultSet.getNString("title");
+            System.out.println(result);
+            assertEquals("testpw", result);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
 
     }
 }
