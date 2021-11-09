470c6df36b718c2e834522cdf97754262e5a2c0a
rst0070 <kwb0711@gmail.com>
2021-11-09 11:02:41 +0000

    test for issue#4

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ app.post("/memo/create", (req, res) => {
     console.log(req.body.content);
     console.log(req.body.tagList);
 })
+
+app.post("/hi", (req, res)=>{
+    console.log(req.body.name);
+    res.status(200).json({ data: 'message' })
+})
 app.listen(3000, ()=>{
     console.log('server started');
 });
